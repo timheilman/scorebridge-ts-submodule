3eae4a24e9e829d2a9a51f2c1e07b7a1c57ffc20
Tim Heilman <tdh@stanfordalumni.org>
2023-09-01 12:38:49 -0700

    refactor: generify aws sdk client cache

--- cachedCognitoIdpClient.ts
+++ cachedCognitoIdpClient.ts
@@ -1,7 +1,6 @@
 import { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
-import { fromEnv } from "@aws-sdk/credential-providers";
 
-import fromSsoUsingProfile from "./fromSsoUsingProfile";
+import { cachedAwsSdkV3Client } from "./cachedAwsSdkV3Client";
 
 const profileDict: {
   [awsRegion: string]: { [profile: string]: CognitoIdentityProviderClient };
@@ -10,29 +9,15 @@ const envDict: {
   [awsRegion: string]: CognitoIdentityProviderClient;
 } = {};
 
-export default function cachedCognitoIdpClient(
+export const cachedCognitoIdpClient = (
   awsRegion: string,
   profile: string | null,
-) {
-  if (profile) {
-    if (profileDict[awsRegion] && profileDict[awsRegion][profile]) {
-      return profileDict[awsRegion][profile];
-    }
-    if (!profileDict[awsRegion]) {
-      profileDict[awsRegion] = {};
-    }
-    profileDict[awsRegion][profile] = new CognitoIdentityProviderClient({
-      region: awsRegion,
-      credentials: fromSsoUsingProfile(profile),
-    });
-    return profileDict[awsRegion][profile];
-  }
-  if (envDict[awsRegion]) {
-    return envDict[awsRegion];
-  }
-  envDict[awsRegion] = new CognitoIdentityProviderClient({
-    region: awsRegion,
-    credentials: fromEnv(),
-  });
-  return envDict[awsRegion];
-}
+): CognitoIdentityProviderClient => {
+  return cachedAwsSdkV3Client<CognitoIdentityProviderClient>(
+    CognitoIdentityProviderClient,
+    awsRegion,
+    profile,
+    profileDict,
+    envDict,
+  );
+};
