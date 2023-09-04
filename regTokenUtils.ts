d5f808dbce1af9ceb9cecc43af048409ca901e80
Tim Heilman <tdh@stanfordalumni.org>
2023-09-04 13:23:45 -0700

    build: oh heavens old js cannot handle it

--- regTokenUtils.ts
+++ regTokenUtils.ts
@@ -19,12 +19,9 @@ export function regTokenToEmail(regToken: string, stage: string) {
 }
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export function randomRegToken() {
-  return [...Array(16).keys()]
-    .map(
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      (_s) => {
-        return characters.charAt(Math.floor(Math.random() * characters.length));
-      },
-    )
+  return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    .map(() => {
+      return characters.charAt(Math.floor(Math.random() * characters.length));
+    })
     .join("");
 }
