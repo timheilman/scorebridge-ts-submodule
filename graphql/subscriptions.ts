9ac4fca9f4cc5ddb0c1a50bd71f1582e34e153d1
Tim Heilman <tdh@stanfordalumni.org>
2023-09-05 10:44:29 -0700

    refactor: move graphql code to shared

---
+++ graphql/subscriptions.ts
@@ -0,0 +1,38 @@
+import gql from "graphql-tag";
+
+export const subscriptionCreatedClubDevice = gql`
+  subscription CreatedClubDevice($clubId: String!) {
+    createdClubDevice(clubId: $clubId) {
+      clubDeviceId
+      clubId
+      createdAt
+      email
+      name
+      updatedAt
+    }
+  }
+`;
+
+export const subscriptionDeletedClubDevice = gql`
+  subscription DeletedClubDevice($clubId: String!) {
+    deletedClubDevice(clubId: $clubId) {
+      clubDeviceId
+      clubId
+      createdAt
+      email
+      name
+      updatedAt
+    }
+  }
+`;
+
+export const subscriptionUpdatedClub = gql`
+  subscription UpdatedClub($clubId: String!) {
+    updatedClub(clubId: $clubId) {
+      id
+      name
+      createdAt
+      updatedAt
+    }
+  }
+`;
