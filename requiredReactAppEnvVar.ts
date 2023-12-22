dc0bb00e8f81d2c266add7b016684b8ac6ebbe5e
Tim Heilman <tdh@stanfordalumni.org>
2023-12-22 09:41:18 -0800

    chore: SCOR-143 remove requiredEnvVar and requiredReactAppEnvVar

--- requiredReactAppEnvVar.ts
+++
@@ -1,8 +0,0 @@
-import requiredEnvVar from "./requiredEnvVar";
-
-// SCOR-143 this function never needed to be in the submodule, so it can move directly to
-// webapp. Furthermore with the move to vite and import.meta.env, it no longer will depend
-// on requiredEnvVar.
-export default function requiredReactAppEnvVar(key: string): string {
-  return requiredEnvVar(`REACT_APP_${key}`);
-}
