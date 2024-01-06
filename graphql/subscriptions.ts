bc546f2aad78c84c0a75ff731e6520bf85c6ecde
Tim Heilman <tdh@stanfordalumni.org>
2024-01-06 13:05:08 -0800

    chore: reverse approach of having __subscriptionName in the same object
    as the gql itself; use subIdToSubGql instead

--- graphql/subscriptions.ts
+++ graphql/subscriptions.ts
@@ -13,99 +13,80 @@ export type GeneratedSubscription<InputType, OutputType> = string & {
 
 export type SubscriptionNames = keyof Omit<Subscription, "__typename">;
 
-export interface KeyedGeneratedSubscription<
+export type KeyedGeneratedSubscription<
   SubscriptionName extends SubscriptionNames,
   InputType,
-> {
-  __subscriptionName: SubscriptionName;
-  gql: GeneratedSubscription<InputType, Pick<Subscription, SubscriptionName>>;
-}
+> = GeneratedSubscription<InputType, Pick<Subscription, SubscriptionName>>;
 
 export const createKeyedGeneratedSubscription = <
   SubscriptionName extends SubscriptionNames,
   InputType,
 >(
-  subName: SubscriptionName,
   subGql: string,
 ): KeyedGeneratedSubscription<SubscriptionName, InputType> => {
-  return {
-    __subscriptionName: subName,
-    gql: subGql as GeneratedSubscription<
-      InputType,
-      Pick<Subscription, SubscriptionName>
-    >,
-  };
+  return subGql as GeneratedSubscription<
+    InputType,
+    Pick<Subscription, SubscriptionName>
+  >;
 };
 export const subscriptionOnCreateClubDevice = createKeyedGeneratedSubscription<
   "onCreateClubDevice",
   SubscriptionOnCreateClubDeviceArgs
->(
-  "onCreateClubDevice",
-  /* GraphQL */ `
-    subscription OnCreateClubDevice($clubId: String!) {
-      onCreateClubDevice(clubId: $clubId) {
-        clubDeviceId
-        clubId
-        createdAt
-        email
-        name
-        updatedAt
-      }
+>(/* GraphQL */ `
+  subscription OnCreateClubDevice($clubId: String!) {
+    onCreateClubDevice(clubId: $clubId) {
+      clubDeviceId
+      clubId
+      createdAt
+      email
+      name
+      updatedAt
     }
-  `,
-);
+  }
+`);
 export const subscriptionOnDeleteClubDevice = createKeyedGeneratedSubscription<
   "onDeleteClubDevice",
   SubscriptionOnDeleteClubDeviceArgs
->(
-  "onDeleteClubDevice",
-  /* GraphQL */ `
-    subscription OnDeleteClubDevice($clubId: String!) {
-      onDeleteClubDevice(clubId: $clubId) {
-        clubDeviceId
-        clubId
-        createdAt
-        email
-        name
-        updatedAt
-      }
+>(/* GraphQL */ `
+  subscription OnDeleteClubDevice($clubId: String!) {
+    onDeleteClubDevice(clubId: $clubId) {
+      clubDeviceId
+      clubId
+      createdAt
+      email
+      name
+      updatedAt
     }
-  `,
-);
+  }
+`);
 
 export const subscriptionOnUpdateClub = createKeyedGeneratedSubscription<
   "onUpdateClub",
   SubscriptionOnUpdateClubArgs
->(
-  "onUpdateClub",
-  /* GraphQL */ `
-    subscription OnUpdateClub($id: String!) {
-      onUpdateClub(id: $id) {
-        id
-        name
-        createdAt
-        updatedAt
-      }
+>(/* GraphQL */ `
+  subscription OnUpdateClub($id: String!) {
+    onUpdateClub(id: $id) {
+      id
+      name
+      createdAt
+      updatedAt
     }
-  `,
-);
+  }
+`);
 
 export const subscriptionOnUpdateClubDevice = createKeyedGeneratedSubscription<
   "onUpdateClubDevice",
   SubscriptionOnUpdateClubDeviceArgs
->(
-  "onUpdateClubDevice",
-  /* GraphQL */ `
-    subscription OnUpdateClubDevice($clubId: String!, $clubDeviceId: String) {
-      onUpdateClubDevice(clubId: $clubId, clubDeviceId: $clubDeviceId) {
-        clubId
-        clubDeviceId
-        name
-        email
-        table
-        createdAt
-        updatedAt
-      }
+>(/* GraphQL */ `
+  subscription OnUpdateClubDevice($clubId: String!, $clubDeviceId: String) {
+    onUpdateClubDevice(clubId: $clubId, clubDeviceId: $clubDeviceId) {
+      clubId
+      clubDeviceId
+      name
+      email
+      table
+      createdAt
+      updatedAt
     }
-  `,
-);
+  }
+`);
