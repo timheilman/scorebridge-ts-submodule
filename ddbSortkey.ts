d87918d61843bb7b92242ec0aa5ea583d9aeee4e
Tim Heilman <tdh@stanfordalumni.org>
2024-02-13 09:34:55 -0800

    feat: ddbSortKey additions for board results

--- ddbSortkey.ts
+++ ddbSortkey.ts
@@ -1,7 +1,11 @@
+import { DirectionLetter } from "./graphql/appsync";
+
 export const clubSortKeyPrefix0 = "C";
 export const clubDeviceSortKeyPrefix0 = "D";
 export const gameSortKeyPrefix0 = "G";
 export const userSortKeyPrefix0 = "U";
+// shares slot with PlayerAssignment.directionLetter, so must not be N, S, E, or W:
+export const resultSortKeyPrefix3 = "R";
 
 export const clubDeviceIdFromSortKey = (sortKey: string) => {
   if (!sortKey.startsWith(clubDeviceSortKeyPrefix0)) {
@@ -25,7 +29,7 @@ export const directionLetterFromSortKey = (sortKey: string) => {
   if (!sortKey.startsWith(gameSortKeyPrefix0)) {
     return {
       error: `Not a correct sortKey; ${sortKey} did not start with ${gameSortKeyPrefix0}`,
-      result: "",
+      result: DirectionLetter.N,
     };
   }
   const theSplit = sortKey.split("#");
@@ -33,10 +37,19 @@ export const directionLetterFromSortKey = (sortKey: string) => {
   if (theSplit.length < 4) {
     return {
       error: `Not a correct sortKey; ${sortKey} has under three hashes.`,
-      result: "",
+      result: DirectionLetter.N,
     };
   }
-  return { result: theSplit[3] };
+  const directionString = theSplit[3];
+  if (!["N", "S", "E", "W"].includes(directionString)) {
+    return {
+      error: `Not a correct sortKey; ${directionString} is not a direction.`,
+      result: DirectionLetter.N,
+    };
+  }
+  return {
+    result: DirectionLetter[directionString as keyof typeof DirectionLetter],
+  };
 };
 
 export const gameIdFromSortKey = (sortKey: string) => {
@@ -73,4 +86,30 @@ export const tableNumberFromSortKey = (sortKey: string) => {
     };
   }
   return { result: +theSplit[2] };
+};
+
+export const boardFromSortKey = (sortKey: string) => {
+  if (!sortKey.startsWith(gameSortKeyPrefix0)) {
+    return {
+      error: `Not a correct sortKey; ${sortKey} did not start with ${gameSortKeyPrefix0}`,
+      result: 0,
+    };
+  }
+  const theSplit = sortKey.split("#");
+  // G, gameId, tableNumber, R, boardNumber
+  if (theSplit.length < 5) {
+    return {
+      error: `Not a correct sortKey; ${sortKey} has under four hashes.`,
+      result: 0,
+    };
+  }
+  if (theSplit[3] !== resultSortKeyPrefix3) {
+    return {
+      error: `Not a correct sortKey; ${sortKey[3]} is not resultSortKeyPrefix3: ${resultSortKeyPrefix3}`,
+      result: 0,
+    };
+  }
+  return {
+    result: +theSplit[4],
+  };
 };
