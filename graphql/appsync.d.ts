9115c31f57ad92d56447a94d6bbf99acbe429c31
Tim Heilman <tdh@stanfordalumni.org>
2024-02-20 10:35:13 -0800

    chore: changed mind, everything sub-club should list with paging

--- graphql/appsync.d.ts
+++ graphql/appsync.d.ts
@@ -108,7 +108,6 @@ export interface ClearCurrentGameIdResponse {
 
 export interface Club {
   __typename?: "Club";
-  clubDevices: ClubDevice[];
   createdAt: Scalars["AWSDateTime"]["output"];
   currentGameId?: Maybe<Scalars["String"]["output"]>;
   id: Scalars["String"]["output"];
