82d8890cd1936c382a671baa4bdf1ca642a718af
Tim Heilman <tdh@stanfordalumni.org>
2024-02-28 06:44:30 -0800

    fix: oops failed to add matchPointsScore

---
+++ matchPointsScore.ts
@@ -0,0 +1,101 @@
+// import { logFn } from "../../utils/logging";
+import {
+  whereWasI,
+  withEachPlayer,
+} from "../features/assignBoardResult/movementHelpers";
+import { boardScoreFromBoardResult } from "./boardScore";
+import { BoardResult } from "./graphql/appsync";
+// const log = logFn("features.gameOver.test.");
+
+interface TrueOpponentsParams {
+  roundCount: number;
+  boardsPerRound: number;
+  playerNumber: number;
+  tableCount: number;
+  board: number;
+  movement: string;
+}
+
+export const trueOpponents = (params: TrueOpponentsParams) => {
+  const whereWasIResult = whereWasI(params)!;
+  const {
+    tableNumber: playerTable,
+    direction: playerDir,
+    round: playerRound,
+  } = whereWasIResult;
+  // log("whereWasI", "debug", { whereWasIResult });
+  return withEachPlayer(params).reduce((acc, otherPlayer) => {
+    const whereWasHe = whereWasI({ ...params, playerNumber: otherPlayer })!;
+    const {
+      tableNumber: otherTable,
+      direction: otherDir,
+      round: otherRound,
+    } = whereWasHe;
+    // log("whereWasHe", "debug", { otherPlayer, whereWasHe });
+    if (otherTable === playerTable && playerRound === otherRound) {
+      return acc;
+    }
+    if (otherDir !== playerDir) {
+      return acc;
+    }
+    acc.push(otherPlayer);
+    return acc;
+  }, [] as number[]);
+};
+
+export const mpScoreCalc = ({
+  myScore,
+  opponentsScores,
+}: {
+  myScore: number;
+  opponentsScores: number[];
+}) => {
+  return (
+    opponentsScores.reduce((acc, opponentScore) => {
+      return (
+        acc + (opponentScore === myScore ? 1 : opponentScore < myScore ? 2 : 0)
+      );
+    }, 0) /
+    opponentsScores.length /
+    2
+  );
+};
+
+export const matchPointsScore = (params: {
+  roundCount: number;
+  boardsPerRound: number;
+  playerNumber: number;
+  tableCount: number;
+  board: number;
+  movement: string;
+  boardResults: Record<string, Omit<Omit<BoardResult, "board">, "round">>;
+}) => {
+  const { board, boardResults } = params;
+  const { tableNumber: playerTable, round: playerRound } = whereWasI(params)!;
+  const whereOpponentsWere = trueOpponents(params).map(
+    (o) => whereWasI({ ...params, playerNumber: o })!,
+  );
+  const myScore = boardScoreFromBoardResult({
+    board,
+    round: playerRound,
+    ...boardResults[`${playerTable}_${board}_${playerRound}`],
+  });
+  if (!myScore) {
+    return myScore;
+  }
+  const opponentsScores = whereOpponentsWere.reduce((acc, opponent) => {
+    const otherResult = boardScoreFromBoardResult({
+      board,
+      round: opponent.round,
+      ...boardResults[`${opponent.tableNumber}_${board}_${opponent.round}`],
+    });
+    if (otherResult !== null && otherResult !== undefined) {
+      acc.push(otherResult);
+    }
+    return acc;
+  }, [] as number[]);
+  if (opponentsScores.length === 0) {
+    return;
+  }
+  return mpScoreCalc({ myScore, opponentsScores });
+};
