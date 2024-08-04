9b7ef6ba5903795f30c0ddea935848e27295f4a7
Tim Heilman <tdh@stanfordalumni.org>
2024-08-04 15:22:22 -0700

    refactor: scor-191 move shallowEqualWithNull to submodule

---
+++ shallowEqualWithNull.ts
@@ -0,0 +1,26 @@
+export const shallowEqualWithNull = <T>(
+  obj1: Record<string, T>,
+  obj2: Record<string, T>,
+) => {
+  const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])];
+
+  for (const key of keys) {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+
+    if (val1 === val2) {
+      continue;
+    }
+
+    if (
+      (val1 === undefined || val1 === null) &&
+      (val2 === undefined || val2 === null)
+    ) {
+      continue;
+    }
+
+    return false;
+  }
+
+  return true;
+};
