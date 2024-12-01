5d47441a768fcf63a5b28b9aaaf6d85f446da23a
Tim Heilman <tdh@stanfordalumni.org>
2024-12-01 06:59:47 -0800

    chore: scor-301 utility methods for translations

--- allDirections.ts
+++ allDirections.ts
@@ -1,1 +1,35 @@
+import { Strain } from "./graphql/appsync";
+
 export const allDirections = ["N", "W", "E", "S"] as const;
+export const englishNameOfDirection = (
+  direction: (typeof allDirections)[number],
+) => {
+  switch (direction) {
+    case "N":
+      return "North";
+    case "W":
+      return "West";
+    case "E":
+      return "East";
+    case "S":
+      return "South";
+    default:
+      return "Unknown";
+  }
+};
+export const englishNameOfStrain = (strain: Strain) => {
+  switch (strain) {
+    case "C":
+      return "Clubs";
+    case "D":
+      return "Diamonds";
+    case "H":
+      return "Hearts";
+    case "S":
+      return "Spades";
+    case "NT":
+      return "No Trump";
+    default:
+      return "Unknown";
+  }
+};
