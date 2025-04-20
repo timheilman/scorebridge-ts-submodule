9f4654041f0d528f1e0ac0def5dfa86f42113cda
Tim Heilman <tdh@stanfordalumni.org>
2025-04-20 13:46:17 -0700

    test: boh-373 provide means to turn off gql promise retries during slow tests

--- react/messageErroringGql.ts
+++ react/messageErroringGql.ts
@@ -1,7 +1,16 @@
 import { tsSubmoduleLogFn } from "../tsSubmoduleLog";
 import { retryOnTimeoutGqlPromise } from "./reactPromiseUtils";
 const log = tsSubmoduleLogFn("messageErroringGql.");
+let enableRetryOnGqlTimeout = true;
+
+export const setEnableRetryOnGqlTimeout = (enable: boolean) => {
+  enableRetryOnGqlTimeout = enable;
+};
 export const messageErroringGql = async <T>(gqlPromiseFn: () => Promise<T>) => {
+  if (!enableRetryOnGqlTimeout) {
+    log("messageErroringGql.directPassThrough", "debug");
+    return gqlPromiseFn();
+  }
   try {
     return await retryOnTimeoutGqlPromise({ gqlPromiseFn });
   } catch (e) {
