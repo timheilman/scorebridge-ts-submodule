92945318e900d95cee5de474f63f730318194aae
Tim Heilman <tdh@stanfordalumni.org>
2025-07-12 19:27:47 -0700

    fix: scor-480 add undefined to potential type for cached client

--- react/gqlClient.ts
+++ react/gqlClient.ts
@@ -1,5 +1,7 @@
 import { generateClient } from "aws-amplify/api";
-let client: ReturnType<typeof generateClient<Record<string, unknown>>>;
+let client:
+  | ReturnType<typeof generateClient<Record<string, unknown>>>
+  | undefined = undefined;
 export const cachedClient = () => {
   client ??= generateClient<Record<string, unknown>>();
   return client;
