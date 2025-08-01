cbae1c434cb16ee763523306ca8b3d458afe2546
Tim Heilman <tdh@stanfordalumni.org>
2025-08-01 08:21:33 -0700

    feat: scor-61 add cachedIamClient

---
+++ cachedIamClient.ts
@@ -0,0 +1,19 @@
+import { IAMClient } from "@aws-sdk/client-iam";
+
+import { cachedAwsSdkV3Client } from "./cachedAwsSdkV3Client";
+
+const profileDict: Record<string, Record<string, IAMClient>> = {};
+const envDict: Record<string, IAMClient> = {};
+
+export const cachedIamClient = (
+  awsRegion: string,
+  profile: string | null,
+): IAMClient => {
+  return cachedAwsSdkV3Client<IAMClient>(
+    IAMClient,
+    awsRegion,
+    profile,
+    profileDict,
+    envDict,
+  );
+};
