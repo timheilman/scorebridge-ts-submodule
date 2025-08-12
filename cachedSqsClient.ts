47988366e73b5588ba8bb6de7cdd9161fbc20845
Tim Heilman <tdh@stanfordalumni.org>
2025-08-12 10:05:01 -0700

    fix: scor-538 add cachedSqsClient

---
+++ cachedSqsClient.ts
@@ -0,0 +1,22 @@
+import { SQSClient } from "@aws-sdk/client-sqs";
+
+import { cachedAwsSdkV3Client } from "./cachedAwsSdkV3Client";
+
+const profileDict: Record<string, Record<string, SQSClient>> = {};
+const envDict: Record<string, SQSClient> = {};
+
+export const cachedSqsClient = ({
+  awsRegion,
+  profile = null,
+}: {
+  awsRegion: string;
+  profile?: string | null;
+}): SQSClient => {
+  return cachedAwsSdkV3Client<SQSClient>(
+    SQSClient,
+    awsRegion,
+    profile,
+    profileDict,
+    envDict,
+  );
+};
