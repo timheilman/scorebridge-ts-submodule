f31724648f11c9f210757313898d04dd2be8156b
Tim Heilman <tdh@stanfordalumni.org>
2026-01-29 15:18:11 -0800

    build: scor-565 remove leading virgule from signin and signout suffixes

--- amplifyConfigRedirectsSuffixes.ts
+++ amplifyConfigRedirectsSuffixes.ts
@@ -1,4 +1,4 @@
 // Expo-router-based routing serves app/(auth)/finishingSignIn at:
-export const signInSuffix = "/finishingSignIn";
+export const signInSuffix = "finishingSignIn";
 // Expo-router-based routing serves app/(user)/finishingSignOut at:
-export const signOutSuffix = "/finishingSignOut";
+export const signOutSuffix = "finishingSignOut";
